7f0e7ad863ae562aab37ee225ad8d416912d249c
Raul Luna <superman_ha_muerto@yahoo.com>
2024-06-12 23:58:53 +0200

    added first component

--- projects/mobile/src/app/app.config.ts
+++ projects/mobile/src/app/app.config.ts
@@ -5,7 +5,9 @@ import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+            provideRouter(routes), 
+            provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
           })]
